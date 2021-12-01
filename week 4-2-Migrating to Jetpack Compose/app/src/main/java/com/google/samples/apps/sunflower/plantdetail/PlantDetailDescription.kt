75bc9f676748bdc58c03f493830f52ae54cdfcdd
wootaeng <karfuci@gmail.com>
2021-12-01 15:03:44 +0900

    compose 4-2 commit

--- week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ week 4-2-Migrating to Jetpack Compose/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,13 +16,160 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.content.res.Configuration
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+import com.google.android.material.composethemeadapter.MdcTheme
+import com.google.samples.apps.sunflower.R
+import com.google.samples.apps.sunflower.data.Plant
+import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 
 @Composable
-fun PlantDetailDescription() {
+private fun PlantName(name: String) {
+    Text(
+        text = name,
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
+}
+
+@Composable
+//viewmodel 연결
+fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
+    // viewmodel 의 LiveData<Plant> 필드로 부터 들어오는 값을 관찰한다.
+    val plant by plantDetailViewModel.plant.observeAsState()
+
+    // plant 가 null 이 아니면, 내용을 표시
+    plant?.let {
+        PlantDetailContent(it)
+    }
+//    Text("Hello Compose")
+
+}
+
+@Composable
+fun PlantDetailContent(plant: Plant) {
     Surface {
-        Text("Hello Compose")
+        Column(Modifier.padding(dimensionResource(R.dimen.margin_normal))) {
+            PlantName(plant.name)
+            PlantWatering(plant.wateringInterval)
+            PlantDescription(plant.description)
+        }
+    }
+}
+
+@Composable
+private fun PlantDescription(description: String) {
+    // HTML 형식의 description을 내부에 저장한다. 새로운 description이 들어오면 람다식을 재실행한다.
+    val htmlDescription = remember(description) {
+        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    }
+
+    // 화면상에 TextView를 표시하고 전개 될 때 HTML description을 업데이트 한다.
+    // htmlDescription을 업데이트 하는 것은 AndroidView를 재구성하도록 만들고, 텍스트를 업데이트 시킨다.
+    AndroidView(
+        factory = { context ->
+            TextView(context).apply {
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+        },
+        update = {
+            it.text = htmlDescription
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun PlantDescriptionPreview() {
+    MdcTheme() {
+        PlantDescription("HTML<br><br>description")
+    }
+}
+
+@Preview
+@Composable
+private fun PlantDetailContentPreview() {
+
+    val plant = Plant("id", "Apple", "description", 3, 30, "")
+    MdcTheme() {
+        PlantDetailContent(plant)
+    }
+}
+
+@Composable
+private fun PlantWatering(wateringInterval: Int){
+    Column(Modifier.fillMaxWidth()) {
+        // 두 Text 에 같은 modifier 가 사용
+        val centerWithPaddingModifier = Modifier
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .align(Alignment.CenterHorizontally)
+
+        val normalPadding = dimensionResource(R.dimen.margin_normal)
+
+        Text(
+            text = stringResource(R.string.watering_needs_prefix),
+            color = MaterialTheme.colors.primaryVariant,
+            fontWeight = FontWeight.Bold,
+            modifier = centerWithPaddingModifier.padding(top = normalPadding)
+        )
+
+        val wateringIntervalText = LocalContext.current.resources.getQuantityString(
+            R.plurals.watering_needs_suffix, wateringInterval, wateringInterval
+        )
+        Text(
+            text = wateringIntervalText,
+            modifier = centerWithPaddingModifier.padding(bottom = normalPadding)
+        )
+
+    }
+}
+
+@Preview
+@Composable
+private fun PlantWateringPreview() {
+    MdcTheme() {
+        PlantWatering(7)
+    }
+}
+
+
+@Preview
+@Composable
+private fun PlantNamePreview() {
+    MdcTheme() {
+        PlantName("Apple")
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun PlantDetailContentDarkPreview() {
+    val plant = Plant("id", "Apple", "HTML<br><br>description", 3, 30, "")
+    MdcTheme {
+        PlantDetailContent(plant)
     }
 }
